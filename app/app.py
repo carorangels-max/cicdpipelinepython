7f7e136af40944d08ff8e4a3d20e399419a0915f
carorangels-max <carorangels@gmail.com>
2025-09-18 20:08:20 -0500

    Implement CI pipeline with Docker build1

--- app/app.py
+++ app/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
+
+
+## Este módulo contiene funciones matemáticas básicas como suma, resta, multiplicación y división. ##
+
 def index():
     resultado = None
     if request.method == "POST":
