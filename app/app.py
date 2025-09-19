385979541c6a0de6f947d438cd80f6993b13d394
carorangels-max <carorangels@gmail.com>
2025-09-18 21:03:43 -0500

    Implement CI pipeline with Docker build1

--- app/app.py
+++ app/app.py
@@ -12,34 +12,27 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    """
-    Maneja la ruta principal de la aplicación.
-    Recibe datos desde un formulario web, realiza la operación matemática seleccionada
-    (suma, resta, multiplicación o división) y devuelve el resultado renderizado en la plantilla HTML.
-    """
-
-
-resultado = None
-if request.method == "POST":
-    try:
-        num1 = float(request.form["num1"])
-        num2 = float(request.form["num2"])
-        operacion = request.form["operacion"]
-        if operacion == "sumar":
-            resultado = sumar(num1, num2)
-        elif operacion == "restar":
-            resultado = restar(num1, num2)
-        elif operacion == "multiplicar":
-            resultado = multiplicar(num1, num2)
-        elif operacion == "dividir":
-            resultado = dividir(num1, num2)
-        else:
-            resultado = "Operación no válida"
-    except ValueError:
-        resultado = "Error: Introduce números válidos"
-    except ZeroDivisionError:
-        resultado = "Error: No se puede dividir por cero"
-        return render_template("index.html", resultado=resultado)
+    resultado = None
+    if request.method == "POST":
+        try:
+            num1 = float(request.form["num1"])
+            num2 = float(request.form["num2"])
+            operacion = request.form["operacion"]
+            if operacion == "sumar":
+                resultado = sumar(num1, num2)
+            elif operacion == "restar":
+                resultado = restar(num1, num2)
+            elif operacion == "multiplicar":
+                resultado = multiplicar(num1, num2)
+            elif operacion == "dividir":
+                resultado = dividir(num1, num2)
+            else:
+                resultado = "Operación no válida"
+        except ValueError:
+            resultado = "Error: Introduce números válidos"
+        except ZeroDivisionError:
+            resultado = "Error: No se puede dividir por cero"
+    return render_template("index.html", resultado=resultado)
 
 
 if __name__ == "__main__":  # pragma: no cover
