539ec57ad30a40015170be4880a81c3821b678d3
carorangels-max <carorangels@gmail.com>
2025-09-18 21:20:09 -0500

    Implement CI pipeline with Docker build1

--- app/calculadora.py
+++ app/calculadora.py
@@ -11,7 +11,20 @@ def multiplicar(a, b):
     return a * b
 
 
-def dividir(a, b):
+def dividir(a: float, b: float) -> float:
+    """
+    Divide dos números y devuelve el resultado.
+
+    Args:
+        a (float): El numerador.
+        b (float): El denominador.
+
+    Returns:
+        float: El resultado de dividir `a` entre `b`.
+
+    Raises:
+        ZeroDivisionError: Si `b` es cero.
+    """
     if b == 0:
         raise ZeroDivisionError("No se puede dividir por cero")
     return a / b
