c703d0dc825511e4ffca88cc9c7ae57a16df5cb8
carorangels-max <carorangels@gmail.com>
2025-09-18 20:46:25 -0500

    Implement CI pipeline with Docker build1

--- app/app.py
+++ app/app.py
@@ -9,6 +9,7 @@ from .calculadora import sumar, restar, multiplicar, dividir
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     resultado = None
@@ -32,6 +33,7 @@ def index():
         except ZeroDivisionError:
             resultado = "Error: No se puede dividir por cero"
     return render_template("index.html", resultado=resultado)
+
 
 if __name__ == "__main__":  # pragma: no cover
     app.run(debug=True, port=5000, host="0.0.0.0")  # Quita debug=True para producción
