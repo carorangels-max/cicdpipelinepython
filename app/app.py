cf77673e66655b95eeb8a1521b7cda056121b848
carorangels-max <carorangels@gmail.com>
2025-09-20 20:25:52 -0500

    Implement CI pipeline with Docker build1

--- app/app.py
+++ app/app.py
@@ -8,7 +8,6 @@ from flask import Flask, render_template, request
 from .calculadora import sumar, restar, multiplicar, dividir
 
 app = Flask(__name__)
-app_port = int(os.environ.get("PORT", 5000))
 
 
 @app.route("/", methods=["GET", "POST"])
